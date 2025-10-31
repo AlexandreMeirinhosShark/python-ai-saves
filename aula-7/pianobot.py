cbc5895313c82386cfafe00041e79fe3c2172c31
AlexandreMeirinhosShark <shark05980@sharkcoders.pt>
2025-10-31 19:57:49 +0000

    Update pianobot.py

--- aula-7/pianobot.py
+++ aula-7/pianobot.py
@@ -11,13 +11,15 @@ select = [1, 2, 3, 4]
 def keypos(event):
     global TECLAS, select
     x, y = gui.position()
-    TECLAS[seon.get()] = (x, y)
-    msg.showinfo(None, f"Posição {select} adquirida.")
+    TECLAS[seon.get()-1] = (x, y)
+    msg.showinfo(None, f"Posição {seon.get()} adquirida: {x, y}")
 
 def ready():
     global TECLAS
     if None in TECLAS:
         msg.showwarning("Aviso", "Nem todas as posições foram marcadas.\nTente novamente.")
+    else:
+        aks = msg.askyesnocancel("Começar", "Se quiser o FNF, clique Sim, se não clique Não para configurar para piano.")
 
 ask = msg.askokcancel("~Posição do rato", "É preciso definir a posição das áreas de clique antes de começar.\nPrima F para marcar uma posição.")
 
@@ -30,8 +32,13 @@ root.bind("<f>", keypos)
 
 lbl = tk.Label(text="Posições:", font="Arial 17 bold")
 lbl.place(x=10, y=198)
+ittle = tk.Label(text="-»)PianoBot(«-", font="Arial 20 bold")
+ittle.pack(pady=20)
+btn = tk.Button(font="Arial 14 bold", command=ready, text="~Começar~")
+btn.pack(pady=50)
 ddn = tk.OptionMenu(root, seon, *select)
-ddn.place(x=150, y=200)
+ddn.place(x=200, y=200)
 ddn["menu"].config(font="Arial 18 bold")
 
 root.mainloop()
+
