76e01b6585ab85452f08f4839c50f99863e33563
AlexandreMeirinhosShark <shark05980@sharkcoders.pt>
2025-11-09 20:16:30 +0000

    Enhance assistant with new features
    Added functionality for Wikipedia search, YouTube playback, and messaging.

--- aula-8/mariatts.py
+++ aula-8/mariatts.py
@@ -1,10 +1,14 @@
 import pyttsx3 as tts
 import speech_recognition as sr
 from datetime import datetime
+import wikipedia as wiki
+import pywhatkit
+from time import sleep
+import os
 
 def speak(text):
     """Fala o texto indicado — reinicializa engine para evitar bloqueio"""
-    print(f"Assistant: {text}")
+    print(f"Assistente: {text}")
     engine = tts.init()
     voices = engine.getProperty('voices')
     engine.setProperty('rate', 150)
@@ -15,6 +19,7 @@ def speak(text):
     engine.stop()
 
 recognition = sr.Recognizer()
+wiki.set_lang("pt")
 
 with sr.Microphone() as source:
     recognition.adjust_for_ambient_noise(source, duration=1)
@@ -39,6 +44,27 @@ with sr.Microphone() as source:
                 speak("Chamo-me Maria.")
             elif "tempo" in query.lower():
                 speak(f"A hora atual é: {datetime.now().strftime('%H:%M')}.")
+            elif "procura" in query.lower():
+                result = " ".join(w for w in query.lower().split() if w != "procura")
+                speak(f"De acordo com a Wikipédia, {wiki.summary(result, sentences=2)}")
+            elif "toca" in query.lower():
+                song = query.lower().replace("toca", "").strip()
+                speak(f"Tocando {song} no YouTube.")
+                pywhatkit.playonyt(song)
+            elif "mensagem" in query.lower():
+                speak("Qual é o número? ")
+                numb = input("Número: ")
+                if numb:
+                    speak("Enviando mensagem...")
+                    zxa = query.lower().replace("mensagem", "").strip()
+                    pywhatkit.sendwhatmsg_instantly(numb, zxa)
+                    sleep(2)
+                    file_path = "PyWhatKit_DB.txt"
+                    if os.path.exists(file_path):
+                        with open(file_path, "w") as f:
+                            f.write("")
+                else:
+                    speak("O teu número não foi digitado. Tente novamente.")
             else:
                 speak("Desculpa, não ouvi.")
 
@@ -46,8 +72,10 @@ with sr.Microphone() as source:
             print("Voz não detetada, ouvindo outra vez...\n")
             continue
         except sr.UnknownValueError:
-            speak("Desculpa, Não apanhei. Por favor repéte.")
+            speak("Desculpa, Não apanhei. Por favor repita.")
             continue
+        except wiki.exceptions.WikipediaException:
+            speak("Não percebi o que queres procurar. Por favor repita.")
         except Exception as e:
             print(f"Erro: {e}")
             continue
